a9610eb4c6918acd0baa84e4b7906d19cd67e7f1
DoosicEE <strou73@naver.com>
2022-09-26 21:21:24 +0900

    Add #1 회문문자열
    length를 반으로 나누어서 풀이하는건 쉬웠다. 앞에서 풀었던 문제들의 풀이가 도움이 되었다.
    
    equalsIgnoreCase는 처음봤다. 신기하다. equlas만 주구장창 썻었는데...

---
+++ src/String/회문문자열/Main.java
@@ -0,0 +1,53 @@
+package String.회문문자열;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+    설명
+
+    앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
+    문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+    단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+
+    입력
+    첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다.
+
+
+    예시입력
+    gooG
+    예시출력
+    YES
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution2(str));
+    }
+
+    private static String solution(String str) {
+        int len = str.length();
+        // 전부 대문자 또는 소문자로 변환하여 비교한다.
+        str = str.toUpperCase();
+        // 양쪽 끝이 같으면 되므로 길이의 반절만 돌면된다. 홀수일시 가운데는 비교하지 않아도된다. length / 2
+        for (int i = 0; i < (len / 2); i++){
+            // 한번이라도 일치하지않을시 NO를 return
+            if(str.charAt(i) != str.charAt((str.length() - 1) - i)){
+                return "NO";
+            }
+        }
+        return "YES";
+    };
+
+    private static String solution2(String str) {
+        // 빌더의 reverse 를 이용하여 뒤집어서 비교한다.
+        String builder = new StringBuilder(str).reverse().toString();
+        // equalsIgnoreCase 슷 사용하여 대소문자 무시
+        if(str.equalsIgnoreCase(builder)){
+            return "YES";
+        }
+        return "NO";
+    };
+}
