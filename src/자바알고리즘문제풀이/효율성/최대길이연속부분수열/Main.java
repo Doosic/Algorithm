82eb6e7bbb32f9ff0b0e6b9e9f7f44067e0e478b
DoosicEE <strou73@naver.com>
2022-11-15 23:02:09 +0900

    Add #23 최대길이연속부분수열
    내가 풀이한건 나도 이해가 안된다.... ㄷㄷ; 왜 풀렸지? 싶을정도다.
    
    강사풀이의 중점은 앞선 문제들과 같다. rt가 올라가고 lt가 쫒아가는 방식이다.
    1.rt는 순차적으로 증가한다.
    2.0이 나올때마다 카운트해주고 arr[rt]값이 k값보다 크다면 lt의 값을 arr[lt]가 0이 나올때까지 증가시켜준다. lt가 0을 만난다면 cnt는 -1을 해줘 rt가 다시 상승할 수 있게 하고 lt는 1칸 증가하고 마무리한다.
    3.for문이 한번씩 돌때마다 마지막에 항상 max값을 체크해준다.

---
+++ src/자바알고리즘문제풀이/효율성/최대길이연속부분수열/Main.java
@@ -0,0 +1,63 @@
+package 자바알고리즘문제풀이.효율성.최대길이연속부분수열;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(n, k, arr));
+    }
+
+    private static int solution(int n, int k, int[] arr){
+        int answer = 0;
+        int lt = 0;
+        int check = 0;
+
+        for(int rt = 0; rt < n; rt++){
+            if(arr[rt] == 0){
+                check++;
+            }
+
+                while (check > k){
+                    if(arr[lt] == 0){
+                        check--;
+                    }
+                    lt++;
+                }
+            answer = Math.max((rt-lt)+1, answer);
+        }
+        return answer;
+    }
+
+
+   /* private static int solution(int n, int k, int[] arr){
+        int answer = 0;
+        int lt = 0;
+        int check = 0;
+
+        for(int rt = 0; rt < n; rt++){
+            if(arr[rt] == 0){
+                check++;
+            }
+
+            if(check > k){
+                answer = Math.max((rt-1)-lt, answer);
+                while (check > k){
+                    lt++;
+                    if(arr[lt] == 0){
+                        check--;
+                    }
+                }
+            }
+        }
+        return answer;
+    }*/
+}
